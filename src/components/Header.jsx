280d8e837483c86d69feac51132a354c73aed6f0
Warren Brown <warren@katapultmedia.com>
2020-08-05 09:17:52 -0500

    chore: added static post and styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+   <header className="App-header">
+     <ul className="container">
+       <li>Site Title</li>
+     </ul>
+   </header>
+  )
+}
+
+export default Header
